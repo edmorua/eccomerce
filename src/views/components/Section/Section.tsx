5eb4ab115e7b5c580b6283a2044a21094bb3edc6
edmorua <eduardo.r.morua@gmail.com>
2022-11-06 02:42:17 -0600

    addin tab indentation

--- src/views/components/Section/Section.tsx
+++ src/views/components/Section/Section.tsx
@@ -1,38 +1,48 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React from 'react'
+import { Box, Card, CardContent, Typography } from '@mui/material'
+
+const ItemCard = () => {
+	return (
+		<Card variant='outlined'>
+			<CardContent>
+				<Typography> word of the day</Typography>
+			</CardContent>
+		</Card>
+	)
+}
 
 const MainSection = () => {
-  return (
-    <Box sx={{ width: '50%', minHeight: '500px', backgroundColor: '#B9E0FF' }}>
-      <Typography>Main Section</Typography>
-    </Box>
-  );
-};
+	return (
+		<Box sx={{ width: '50%', minHeight: '500px', backgroundColor: '#B9E0FF' }}>
+			<ItemCard />
+		</Box>
+	)
+}
 
 const SecondarySection = () => {
-  return (
-    <Box
-      sx={{
-        width: '50%',
-        minHeight: '500px',
-        backgroundColor: 'rgb(141, 158, 255)',
-      }}
-    >
-      <Typography> Secondary Section</Typography>
-    </Box>
-  );
-};
+	return (
+		<Box
+			sx={{
+				width: '50%',
+				minHeight: '500px',
+				backgroundColor: 'rgb(141, 158, 255)',
+			}}
+		>
+			<Typography> Secondary Section</Typography>
+		</Box>
+	)
+}
 
 export type SectionProps = {
-  sectionId: string;
-};
+	sectionId: string
+}
 
 export const Section = (props: SectionProps) => {
-  const { sectionId } = props;
-  return (
-    <Box id={sectionId} sx={{ display: 'flex', flexDirection: 'row' }}>
-      <MainSection />
-      <SecondarySection />
-    </Box>
-  );
-};
+	const { sectionId } = props
+	return (
+		<Box id={sectionId} sx={{ display: 'flex', flexDirection: 'row' }}>
+			<MainSection />
+			<SecondarySection />
+		</Box>
+	)
+}
